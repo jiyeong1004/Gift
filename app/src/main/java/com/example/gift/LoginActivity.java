81049b1b06f12b94dbe2d4daf25c2685b8779fff
Kim Jiah <jiajia2024@gmail.com>
2020-11-12 03:53:17 +0900

    login 0.1

--- app/src/main/java/com/example/gift/LoginActivity.java
+++ app/src/main/java/com/example/gift/LoginActivity.java
@@ -1,18 +1,64 @@
 package com.example.gift;
 
 import android.content.Intent;
+import android.nfc.Tag;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class LoginActivity extends AppCompatActivity {
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
+public class LoginActivity extends AppCompatActivity {
+    private FirebaseAuth auth;
+    EditText emailTxt, passwordTxt;
+    Button loginBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        emailTxt = findViewById(R.id.emailText);
+        passwordTxt = findViewById(R.id.passwordText);
+        loginBtn = findViewById(R.id.btn_login);
+
+        auth = FirebaseAuth.getInstance();
+
+        loginBtn.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                String email = emailTxt.getText().toString();
+                String password = passwordTxt.getText().toString();
+                auth.signInWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    Toast.makeText(LoginActivity.this, "로그인 성공",
+                                            Toast.LENGTH_SHORT).show();
+                                    FirebaseUser user = auth.getCurrentUser();
+                                    //updateUI(user);
+                                } else {
+                                    Toast.makeText(LoginActivity.this, "로그인 실패",
+                                            Toast.LENGTH_SHORT).show();
+                                    //updateUI(null);
+                                    // ...
+                                }
+
+                                // ...
+                            }
+                        });
+            }
+        });
 
         TextView registerButton = (TextView) findViewById(R.id.registerButton);
         registerButton.setOnClickListener(new View.OnClickListener() {
