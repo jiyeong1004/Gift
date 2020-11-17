853d428a9b3c673b8d81b1a02228ae1bdd72d846
jiyeong1004 <ak1157@naver.com>
2020-11-17 09:07:11 +0900

    main

--- app/src/main/java/com/example/gift/MainActivity.java
+++ app/src/main/java/com/example/gift/MainActivity.java
@@ -1,14 +1,30 @@
 package com.example.gift;
 
+import android.app.TabActivity;
 import android.os.Bundle;
+import android.widget.TabHost;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends TabActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TabHost tabHost = getTabHost();
+
+        TabHost.TabSpec tabSpecHome = tabHost.newTabSpec("Home").setIndicator("메인화면");
+        tabSpecHome.setContent(R.id.linear_home);
+        tabHost.addTab(tabSpecHome);
+
+        TabHost.TabSpec tabSpecUnuse = tabHost.newTabSpec("Unuse").setIndicator("미사용");
+        tabSpecUnuse.setContent(R.id.linear_unuse);
+        tabHost.addTab(tabSpecUnuse);
+
+        TabHost.TabSpec tabSpecUse = tabHost.newTabSpec("Use").setIndicator("사용");
+        tabSpecUse.setContent(R.id.linear_use);
+        tabHost.addTab(tabSpecUse);
+
+        tabHost.setCurrentTab(0);
     }
 }
